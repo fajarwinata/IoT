60e031534f555c957f251c763878c6d3f4de108a
Fajar Winata <fajarwinata@hotmail.com>
2017-05-26 10:26:36 +0700

    Configuration File Header

---
+++ esp_8266/basic/config.h
@@ -0,0 +1,10 @@
+/*
+ * Configuration File
+ * Use this one for including this config
+ * to another C++ File using Arduino IDE
+ * Program by Mr. Fajar W.
+ * Standard Procedural
+ */
+ 
+#define SSID "Your SSID"
+#define SSID_PASSWORD "Your Password Here"
